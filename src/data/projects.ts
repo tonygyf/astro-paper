1df0080c8258eb0c03d1d8055b1ecc913d8de309
tonygyf <140357704+tonygyf@users.noreply.github.com>
2025-08-22 23:35:29 +0800

    增加作品集

---
+++ src/data/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    title: "onlineTeach",
+    description: "这是一个模仿学习通的安卓项目，展示了我的技能。",
+    link: "https://github.com/tonygyf/onlineTeach",
+    image: "/assets/images/project1.svg", // 请替换为实际图片路径
+  },
+  {
+    title: "截屏笔记",
+    description: "这是另一个令人印象深刻的项目，使用",
+    link: "https://github.com/tonygyf/cursor-calculator-demo1",
+    image: "/assets/images/project2.svg", // 请替换为实际图片路径
+  },
+];
